c7c309679745f685fc529780be59b073ed81eb5a
Ronan Guilloux <ronan.guilloux@gmail.com>
2013-03-12 00:30:52 +0100

    installer

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
+# just move .bashrc, in case it was already modified 
+mv ~/.bashrc ~/.bashrc-dist && ln -s ~/dotfiles/.bashrc ~/.bashrc
+echo "done."
+
