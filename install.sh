1c991381c38e57326bc49fe9897d30252336c6bf
Ronan Guilloux <ronan.guilloux@gmail.com>
2013-03-29 15:46:20 +0100

    install-test feedbacks

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
+# move original dotfiles 
+mv ~/.bashrc ~/.bashrc-dist
+mv ~/.vim ~/.vim-dist
+mv ~/.gitconfig ~/.gitconfig.local
+
+# create symlinks
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
-# just move .bashrc, in case it was already modified 
-mv ~/.bashrc ~/.bashrc-dist && ln -s ~/dotfiles/.bashrc ~/.bashrc
-mv ~/.vim ~/.vim-dist && ln -s ~/dotfiles/.vim ~/.vim
 echo "done."
 
