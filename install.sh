6faac32c080e90ab399a4a5024678afa19f80574
Ronan Guilloux <ronan.guilloux@gmail.com>
2014-11-22 13:54:03 +0100

    fixnig install, adding pathes in a separate file

--- install.sh
+++ install.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-# move original dotfiles 
-if [ -f /.zshrc ]
+# move original dotfiles in $HOME
+if [ -f ~/.zshrc ]
 then
     mv -f ~/.zshrc ~/.zshrc-dist
 fi
-if [ -f /.bashrc ]
+
+if [ -f ~/.pathes ]
+then
+    mv -f ~/.pathes ~/.pathes-dist
+fi
+
+if [ -f ~/.bashrc ]
 then
     mv -f ~/.bashrc ~/.bashrc-dist
 fi
-if [ -f /.bash_aliases ]
+if [ -f ~/.bash_aliases ]
 then
     mv -f ~/.bash_aliases ~/.bash_aliases-dist
 fi
-if [ -d /.vim ]
+if [ -d ~/.vim ]
 then
     mv -f ~/.vim ~/.vim-dist
 fi
-if [ -f /.gitignore_global ]
+if [ -f ~/.gitignore_global ]
 then
     mv -f ~/.gitignore_global ~/.gitignore_global-dist
 fi
-if [ -f /.gitconfig ]
+if [ -f ~/.gitconfig ]
 then
     mv -f ~/.gitconfig ~/.gitconfig.local
 fi
@@ -32,6 +38,7 @@ fi
 
 # create symlinks
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.pathes ~/.pathes
 ln -s ~/dotfiles/.zshrc.pre-oh-my-zsh ~/.zshrc.pre-oh-my-zsh
 ln -s ~/dotfiles/.zshrcre- ~/.zshrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
